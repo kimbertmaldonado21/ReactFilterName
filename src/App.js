7da7b90e8e7bc76444346acb14aabee326077b0b
Kimbert Maldonado <kimbert.m.maldonado@gmail.com>
2022-09-08 00:48:03 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card,Alert, InputGroup, Form, FormControl, ListGroup, ListGroupItem } from "react-bootstrap";
 
-function App() {
+import React,{useState,useEffect} from "react";
+
+import Data from './MOCK_DATA.json'
+
+
+
+
+const App = () => {
+  const [Filter, setFilter] = useState("")
+  const setFilterVal = (e) =>{
+    setFilter(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App">
+      <main className="content d-flex justify-content-center align-items-center bg-dark">
+        <Card classname="bg-dark">
+          <Card.Title className="card-header card-outline card-danger">
+            <p className="h1">Search Filter</p>
+            <InputGroup>
+            {/* aria-label="Search"
+            aria-aria-describedby="basic-addon1" */}
+            <Form.Control 
+            placeholder ="Search"
+
+            onChange={setFilterVal}
+            />
+            <InputGroup.Text>
+            <i className="fa fa-search" aria-hidden="true"></i></InputGroup.Text>
+            </InputGroup>
+          </Card.Title>
+          <Card.Body className="h-50 overflow-auto">
+          <ListGroup as="ul">
+            <p className="h1">
+              Names
+            </p>
+            {Data.filter((val)=>{
+              // if(!Filter){
+              //   if(val.first_name.toLowerCase().includes(Filter.toLowerCase())){
+              //     return val
+              //   }
+              // }
+              // return val
+
+              if(Filter == ""){
+                return val
+              }else if(val.first_name.toLowerCase().includes(Filter.toLowerCase())){
+                return val
+              }
+            }).map((val, key)=>{
+              return(
+                <>
+                <ListGroupItem key={key} as="li" >
+                  {val.first_name}
+                </ListGroupItem>
+                </>
+              )
+            })}
+            </ListGroup>
+            
+            
+          </Card.Body>
+
+        </Card>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
